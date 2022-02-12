75abfd70ac3483b02dfa290ad7849cffb2172fee
Anton <anton.tseniukh@gmail.com>
2022-02-12 23:17:17 +0200

    travis

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,18 +1,15 @@
-Camel Java Router Project
-=========================
-
-=== How to build
-
-To build this project use
-
-    mvn install
-
-=== How to run
-
-You can run this example using
-
-    mvn camel:run
-
-=== More information
-
-You can find more information about Apache Camel at the website: http://camel.apache.org/
+Необходимо реализовать консольное CRUD приложение, которое взаимодействует с БД и позволяет выполнять все CRUD операции над сущностями:
+Team(id, name, List<Developer> developers)
+Developer(id, firstName, lastName, List<Skill> skills)
+Skill(id, name)
+TeamStatus (enum ACTIVE, DELETED)
+Требования:
+Все CRUD операции для каждой из сущностей
+Придерживаться подхода MVC
+Для сборки проекта использовать Maven
+Для взаимодействия с БД - Hibernate
+Для конфигурирования Hibernate - аннотации
+Инициализация БД должна быть реализована с помощью flyway
+Сервисный слой приложения должен быть покрыт юнит тестами (junit + mockito)
+Результатом выполнения задания должен быть репозиторий на github, с использованием Travis (https://travis-ci.org/) и отображением статуса сборки проекта.
+Технологии: Java, PostgreSQL, Hibernate, Flyway, Maven,
--- src/main/java/com/hometask/hibernate/controller/DeveloperController.java
+++ src/main/java/com/hometask/hibernate/controller/DeveloperController.java
@@ -1,4 +1,42 @@
 package com.hometask.hibernate.controller;
 
+import com.hometask.hibernate.model.Developer;
+import com.hometask.hibernate.repository.DeveloperRepository;
+import com.hometask.hibernate.repository.hibernate.HiberDeveloperRepoImpl;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class DeveloperController {
+    private final DeveloperRepository repo = new HiberDeveloperRepoImpl();
+
+    public Developer getDeveloper(Integer id) {
+        return repo.getById(id);
+    }
+
+    public void deleteDeveloper(Integer id) {
+        repo.deleteById(id);
+    }
+
+    public Developer updateDeveloper(Integer id, String firstName, String lastName, List skills){
+        List<Developer> developers = getAllDevelopers();
+        Developer developer = developers.get(id);
+        developer.setFirstName(firstName);
+        developer.setLastName(lastName);
+        developer.setSkills(skills);
+        repo.update(developer);
+        return developer;
+    }
+
+    public void createDeveloper(String firstName, String lastName, List skills){
+        Developer developer = new Developer();
+        developer.setFirstName(firstName);
+        developer.setLastName(lastName);
+        developer.setSkills(skills);
+        repo.save(developer);
+    }
+
+    public List<Developer> getAllDevelopers() {
+        return repo.getAll();
+    }
 }
